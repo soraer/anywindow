8b32f4660f7cc10ae16790d7d50a9f0dedb436a1
黄云龙 <hylepik@163.com>
2021-07-28 23:12:42 +0800

    文件压缩

--- src/helper/route.js
+++ src/helper/route.js
@@ -6,6 +6,7 @@ const stat = promisify(fs.stat)
 const readdir = promisify(fs.readdir)
 const config = require("../../config")
 const mimeTypes = require("./mime")
+const compress = require("./compress")
 
 const tplPath = path.join(__dirname, '../template/dir.html')
 const source = fs.readFileSync(tplPath)
@@ -17,7 +18,11 @@ module.exports = async function (req, res, filePath) {
             const contentType = mimeTypes(filePath)
             res.statusCode = "200"
             res.setHeader('Content-Type', contentType)
-            fs.createReadStream(filePath).pipe(res)
+            let rs = fs.createReadStream(filePath)
+            if (filePath.match(config.compress)) {
+                rs = compress(rs, req, res)
+            }
+            rs.pipe(res)
         } else if (stats.isDirectory()) {
             const files = await readdir(filePath)
             res.statusCode = "200"
