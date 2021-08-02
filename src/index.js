359b98a1d6d631f4c80491ed8c2ce9a78c91b9b0
黄云龙 <hylepik@163.com>
2021-08-02 21:35:16 +0800

    添加注释

--- src/index.js
+++ src/index.js
@@ -30,6 +30,7 @@ server.start()
 // 如何把项目弄成npm包？
 // packaeg.json 中配置bin字段属性，配置对应的执行路径
 // 对应的在项目目录里添加bin文件夹和anywindow文件
+// 注意：anywindow中有这么一句，#! /usr/bin/env node，指的是用/usr/bin/env动态的去查找到你的脚本路径，并用node来执行
 // linux里需要给文件添加执行权限
 // 执行 chmod +x bin/anywindow 添加执行权限
 // 此时就可以用 bin/anywindow -p 9999 来启动
